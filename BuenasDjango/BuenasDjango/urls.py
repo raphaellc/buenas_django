ca6bf266d1db25fb2de825ae63281e66bc431004
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2024-09-12 21:17:49 -0300

    BuenasDjango rodando!

--- BuenasDjango/BuenasDjango/urls.py
+++ BuenasDjango/BuenasDjango/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def buenasDjango(request):
+    return HttpResponse('Buenas Django')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('BuenasDjango/',buenasDjango)
 ]
