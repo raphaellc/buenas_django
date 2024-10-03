e9d548b166af75d109dac001905fa24a3a6a89dc
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2024-10-03 20:28:26 -0300

    Pessoa Create Form e View

--- BuenasDjango/primeiroCrud/views.py
+++ BuenasDjango/primeiroCrud/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView, DetailView
+from .models import Pessoa
+from .forms import PessoaCreateForm, PessoaUpdateForm 
+# Create your views here.
+#criação da tela de cadastro de pessoa
+class PessoaCreateView(CreateView):
+    model = Pessoa
+    form_class = PessoaCreateForm
+    template_name = 'cadastrar_pessoa.html'
+    success_url = reverse_lazy('lista_pessoas')
 
 # Create your views here.
